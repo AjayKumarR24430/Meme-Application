30cf9cee5786137a6b5273242b03c7f24173db8e
Ajay Kumar R <ajaykumarr3024@gmail.com>
2021-02-10 12:45:15 +0530

    Initial set up

---
+++ xmeme-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+
+    return (
+      <div>
+        welcome
+      </div>
+    );
+}
+
+export default App;
