6149f7128e3f5ceb5298579f37ed116b24477814
Ajay Kumar R <ajaykumarr3024@gmail.com>
2021-02-20 01:37:27 +0530

    sh file changes

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt update
 
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
-sudo systemctl status mongod
+# sudo systemctl status mongod
 sudo systemctl enable mongod
 
 
--- server_run.sh
+++ server_run.sh
@@ -9,4 +9,4 @@ cd ./xmeme-backend
 
 npm install
 
-npm run
+npm start
